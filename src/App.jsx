e3339a7f74195da9d76cdd515518bc39d2934bc3
tristan <tristan.wermelinger@gmail.com>
2023-05-05 17:33:28 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import logo from "./assets/logo.jpg";
 import React, { useState } from "react";
 import tv from "./assets/m6.json";
+// console.log(tv);
 
 const App = () => {
   return (
@@ -9,15 +10,24 @@ const App = () => {
       <div className="fleximg">
         <img src={logo} alt="" />
       </div>
-      {tv.map((time, index) => {
+
+      {tv.map((obj, index) => {
+        //console.log(obj);
         return (
-          <div key={index}>
-            <div>{tv.time}</div>
-          </div>
+          <>
+            <div key={index}>
+              <div className="flexprogramme">
+                <h3>{obj.time}</h3>
+                <img src={obj.image} alt="" />
+                <p>{obj.title}</p>
+                <p>{obj.type}</p>
+                <p>{obj.duration}</p>
+              </div>
+            </div>
+          </>
         );
       })}
     </>
-    // <p>{data.time}</p>
   );
 };
 
